8de439f90c6894a993ea727fa9bf95668c8f2da0
Yan Manica Oehling <yanmano@gmail.com>
2024-06-15 04:50:47 -0300

    melhorias e uso de excel

--- analise_de_aposta.py
+++ analise_de_aposta.py
@@ -1,42 +1,38 @@
-jogos = open('jogos.txt')
-numeros = jogos.read().replace('\n', ' ').split()
+import pandas 
 
+
+todos_jogos = pandas.read_excel('historico_jogos_ms.xlsx')
 acertos = [0, 0, 0, 0, 0, 0, 0]
-
 aposta = []
 
-quantidade = str(input('Quantos números vc quer apostar? ')).strip()
+quant_aposta = input('Quantos números vc quer apostar? ')
+while not quant_aposta.isnumeric() or int(quant_aposta) not in range(6, 16):
+    quant_aposta = input('Digite uma quantidade válida: ')
 
-while not quantidade.isnumeric() or int(quantidade) not in range(6, 16):
-    quantidade = str(input('\033[31mDigite uma quantidade válida: \033[m')).strip()
-
-for c in range(0, int(quantidade)):
-    num = str(input(f'{c + 1}º número: ')).strip()
-
+for cont in range(int(quant_aposta)):
+    num = input(f'{cont+1}º número: ')
     while not num.isnumeric() or int(num) not in range(1, 61) or num in aposta:
-        num = str(input(f'\033[31mDigite um {c + 1}º número válido: \033[m')).strip()
-    aposta.append(num)
+        num = str(input(f'Digite um {cont+1}º número válido: '))
+
+    aposta.append(int(num))
+
+for linha in range(len(todos_jogos)):
+    nivel = 0
+    for bola in todos_jogos.iloc[linha][1:]:
+        for num in aposta:
+            if num == bola:
+                nivel += 1
+    if nivel == 6:
+        dia = todos_jogos.iloc[linha][0]
+
+    acertos[nivel] += 1
+
 print()
-
-for c in range(len(numeros)):
-    cont = 0
-    if c % 6 == 0:
-        jogo = numeros[c - 5:c + 1]
-        if len(jogo) > 0:
-            for ap in aposta:
-                for jg in jogo:
-                    if ap == jg:
-                        cont += 1
-            acertos[cont] += 1
-
-print(f'Seu jogo: ', end='')
-for x in aposta:
-    print(x, end=' ')
-print()
-print(f'Em {(len(numeros) - 1) / 6:.0f} jogos, você conseguiria:')
-print('-'*32)
-
 nomes = ['Nenhum acerto', '1 acerto', '2 acertos', '3 acertos', 'Quadra', 'Quina', 'Sena']
-print('Você conseguiria ->')
-for c in range(0, 7):
+print(f'Em {len(todos_jogos)} jogos, você conseguiria:')
+print('-'*32)
+for c in range(7):
     print(f'{nomes[c]}: {acertos[c]} vezes')
+if acertos[6] >= 1:
+    print(f'-- Com essa combinação, você ganharia na megasena dia {dia}! --')
+print()
